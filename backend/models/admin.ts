25132ffb7366b544c455024ef89b8561bbf1e258
Divyanshu Raj <divyanshuraj23102005@gmail.com>
2026-05-31 22:16:17 +0530

    feat: refactor admin model and authentication logic, add JWT and bcrypt integration

--- backend/models/admin.ts
+++ backend/models/admin.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-// import jwt from "jsonwebtoken"
 
 const adminSchema = new mongoose.Schema({
     email :{
@@ -12,22 +11,6 @@ const adminSchema = new mongoose.Schema({
     } 
 }, {timestamps : true});
 
-// adminSchema.methods.getJWT = async function() {
-//     const user = this;
-
-//     const token = await jwt.sign({_id : user._id}, "DEV@Tinder123", {expiresIn : "7d"});
-
-//     return token;
-// }
-
-// adminSchema.methods.bcryptCompare = async function(passwordByUser){
-//     const user = this;
-
-//     const hashPass = user.password;
-
-//     const valid = await bcrypt.compare(passwordByUser, hashPass);
-//     return valid;
-// }
 
 const Admin = mongoose.model("Admin", adminSchema);
 
--- backend/route/authRouter.ts
+++ backend/route/authRouter.ts
@@ -1,37 +1,48 @@
 import express from "express";
-import bcrypt from "bcrypt";
 const { validateSinUp } = require('../utils/validate.js');
 const authRouter = express.Router();
 import Admin from "../models/admin.js";
 const validator = require("validator");
 
 
-authRouter.post("/login", async(req, res)=>{
-    try{
-        const {eMail,password} = req.body;
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-        if(!validator.isEmail(eMail)){
-            throw new Error("Email is in the wrong format");
-        }
-        const user = await Admin.findOne({eMail:eMail});
-        if(!user){
-            throw new Error("Invalid Credentials");
-        }
-        const valid = await user.bcryptCompare(password);
-        if(!valid){
-            throw new Error("Inavlid Credentials");   
-        }
-        const token = await user.getJWT();
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { eMail, password } = req.body;
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none"
-        });
-        res.send("Loggin Successful!!");
+    if (!validator.isEmail(eMail)) {
+      throw new Error("Email is in the wrong format");
     }
-    catch (err:any) {
-        console.log(err);
-        res.status(400).send("Error: "+err.message);
+
+    const user = await Admin.findOne({ eMail });
+
+    if (!user) {
+      throw new Error("Invalid Credentials");
     }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Invalid Credentials");
+    }
+
+    const token = jwt.sign(
+      { _id: user._id },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, // true after deployment with HTTPS
+      sameSite: "lax"
+    });
+
+    res.send("Login Successful!!");
+  } catch (err: any) {
+    console.log(err);
+    res.status(400).send("Error: " + err.message);
+  }
 });
